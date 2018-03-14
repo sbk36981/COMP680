cf2cab4ecbb689f5c038931bef9fd91c92e174fe
Garret Richardson <garret.richardson.481@my.csun.edu>
2018-03-14 16:42:55 -0700

    basic link to API gateway is implemented

--- portal/customerportal/views.py
+++ portal/customerportal/views.py
@@ -1,11 +1,37 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from django.shortcuts import render
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+import requests, json
+from django.conf import settings
 
 # Create your views here.
-from django.http import HttpResponse
 
+from .models import House
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the customer portal.")
+    house_list = House.objects.order_by('-request_date')
+    context = {'latest_houses_list' : house_list}
+    return render(request, 'houses/index.html', context)
+
+def housedetail(request, house_id):
+    house = get_object_or_404(House, pk=house_id)
+    return render(request, "houses/detail.html", {'house' : house})
+
+def prediction(request, house_id):
+    house = get_object_or_404(House, pk=house_id)
+    body = {'datain' : house.csv_data}
+    payload = json.dumps(body)
+    endpoint = 'https://e2g8b7xnl6.execute-api.us-west-2.amazonaws.com/development/SingleHousingPrediction'
+    headers = {
+        'Content-Type' : 'application/json',
+        'x-api-key' : settings.X_AMAZON_API_KEY
+    }
+    r = requests.post(endpoint, headers=headers, data=payload)
+    try:
+        house.predicted_price = json.loads(r.content)["predictions"][0]["score"]
+    except KeyError:
+        return HttpResponseRedirect(reverse('customerportal:housedetail', args=[house.id]))
+    house.save()
+    return HttpResponseRedirect(reverse('customerportal:housedetail', args=[house.id]))
