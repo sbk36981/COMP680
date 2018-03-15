1314df96adcdfde5978c6434dfd590a22fa49be1
Garret Richardson <garret.richardson.481@my.csun.edu>
2018-03-14 18:24:13 -0700

    Authentication implemented

--- portal/customerportal/urls.py
+++ portal/customerportal/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from django.urls import path
 
 from . import views
-app_name = 'customerportal'
+app_name = 'cusportal'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     path('<int:house_id>/', views.housedetail, name="housedetail"),
--- portal/portal/urls.py
+++ portal/portal/urls.py
@@ -13,11 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url, include 
+from django.urls import path
+
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^customerportal/', include('customerportal.urls')),
     url(r'^', include('customerportal.urls'))
+]
+
+#Add Django site authentication urls (for login, logout, password management)
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
