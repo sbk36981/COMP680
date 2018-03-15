3b059ac0cb08deceb421be54f66cc64bdf0bc86f
garret richardson <garret@minislayer.localdomain>
2018-03-14 18:24:13 -0700

    Authentication implemented

--- portal/customerportal/views.py
+++ portal/customerportal/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 import requests, json
@@ -10,15 +11,18 @@ from django.conf import settings
 
 from .models import House
 
+@login_required
 def index(request):
     house_list = House.objects.order_by('-request_date')
     context = {'latest_houses_list' : house_list}
     return render(request, 'houses/index.html', context)
 
+@login_required
 def housedetail(request, house_id):
     house = get_object_or_404(House, pk=house_id)
     return render(request, "houses/detail.html", {'house' : house})
 
+@login_required
 def prediction(request, house_id):
     house = get_object_or_404(House, pk=house_id)
     body = {'datain' : house.csv_data}
@@ -32,6 +36,6 @@ def prediction(request, house_id):
     try:
         house.predicted_price = json.loads(r.content)["predictions"][0]["score"]
     except KeyError:
-        return HttpResponseRedirect(reverse('customerportal:housedetail', args=[house.id]))
+        return HttpResponseRedirect(reverse('cusportal:housedetail', args=[house.id]))
     house.save()
-    return HttpResponseRedirect(reverse('customerportal:housedetail', args=[house.id]))
+    return HttpResponseRedirect(reverse('cusportal:housedetail', args=[house.id]))
